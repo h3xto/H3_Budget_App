8490a09f6acd26a4020a0f366c04779caab63271
h3xto <53286254+h3xto@users.noreply.github.com>
2025-09-07 20:23:00 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,12 +14,10 @@ self.addEventListener("install", e=>{
   e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", e=>{
   e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));
   self.clients.claim();
 });
-
 self.addEventListener("fetch", e=>{
   if (e.request.method !== "GET") return;
   e.respondWith(
@@ -32,4 +30,3 @@ self.addEventListener("fetch", e=>{
     )
   );
 });
-
